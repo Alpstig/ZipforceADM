8c7261f99cae786f2ed9428e7c77388defba6147
Krister Johansson <krister.johansson86@outlook.com>
2020-02-06 02:58:46 +0100

    remove

--- ios/Connect/Empty.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Empty.swift
-//  Connect
-//
-//  Created by Krister Johansson on 2020-01-22.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
--- reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import bluetooth from './bluetooth'
-
-export default combineReducers({
-  bluetooth: bluetooth,
-})
